23a45914d7a38570fe0db96511d2b0fe58f2bc45
HasiukI <136508423+HasiukI@users.noreply.github.com>
2024-12-21 14:15:41 +0200

    Add project files.

---
+++ ChronoLog/ViewModel/Components/WindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChronoLog.ViewModel.Components
+{
+    internal class WindowViewModel
+    {
+
+        private ICommand _showWindowCommand;
+        public ICommand ShowWindowCommand
+        {
+            get
+            {
+                if (_showWindowCommand == null)
+                {
+                    _showWindowCommand = new RelayCommand(
+                        () =>
+                        {
+                            Application.Current.MainWindow.WindowState = WindowState.Normal;
+                            Application.Current.MainWindow.Show();
+                            Application.Current.MainWindow.Activate();
+                        });
+                }
+                return _showWindowCommand;
+            }
+        }
+
+        private ICommand _hideWindowCommand;
+        public ICommand HideWindowCommand
+        {
+            get
+            {
+                if (_hideWindowCommand == null)
+                {
+                    _hideWindowCommand = new RelayCommand(() =>
+                    {
+                        Application.Current.MainWindow.Hide();
+                    });
+
+                }
+                return _hideWindowCommand;
+            }
+
+        }
+
+        public ICommand CloseWindowCommand
+        {
+            get => new RelayCommand(() => {
+                Application.Current.Shutdown();
+            });
+        }
+    }
+}
---
+++ ChronoLog/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ChronoLog.ViewModel.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoLog.ViewModel
+{
+    internal class MainViewModel
+    {
+        public WindowViewModel Window { get; set; }
+
+        public MainViewModel() { 
+            Window = new WindowViewModel();
+        }
+    }
+}
